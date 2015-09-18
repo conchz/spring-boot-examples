3bc52516df075f90f87423430acf995bc58f85cf
dolphineor <dolphineor@gmail.com>
2015-09-18 22:42:16 +0800

    [master] Update AbstractId.java

--- src/main/java/com/github/dolphineor/spring/model/entity/AbstractId.java
+++ src/main/java/com/github/dolphineor/spring/model/entity/AbstractId.java
@@ -5,12 +5,14 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 
 /**
  * Created on 2015-09-15.
  *
  * @author dolphineor
  */
+@MappedSuperclass
 public abstract class AbstractId {
 
     @Id
