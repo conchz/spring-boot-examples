1c5120ee35086d9c7dfd00744c0734a037996170
dolphineor <dolphineor@gmail.com>
2015-10-21 19:37:21 +0800

    [master] Update run.sh

--- run.sh
+++ run.sh
@@ -26,7 +26,7 @@ unzip spring-boot-app.zip -d ${TMP_TARGET_DIR}
 cd ${TMP_TARGET_DIR} && cp spring-boot-examples.jar ${HOME}/${DOCKER_DIR}/${SPRING_BOOT_EXAMPLES_DIR}
 
 
-# If specific image not exists, build a new image
+# If specific image not exist, build a new image
 if [ -z "$(docker images -q spring-boot-examples 2> /dev/null)" ];  then
     docker build -t spring-boot-examples .
 fi
@@ -37,7 +37,7 @@ CONTAINER=spring-boot-web
 # Container status
 RUNNING=$(docker inspect --format="{{ .State.Running }}" ${CONTAINER} 2> /dev/null)
 
-# If specific container not exists, run a new container
+# If specific container not exist, run a new container
 if [ $? -eq 1 ];  then
     docker run --name ${CONTAINER} -d -p 8088:8081 -v ${HOME}/${DOCKER_DIR}/${SPRING_BOOT_EXAMPLES_DIR}:/opt/spring-boot-examples spring-boot-examples /bin/bash
 fi
